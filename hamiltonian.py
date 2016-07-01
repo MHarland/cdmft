fix weiss field definition by sign; add model MomentumPlaquetteBethe; add transformation module for matrix transformations and resorting blockstructure

hamiltonian.py
@@ -1,6 +1,8 @@
-from pytriqs.operators import c as C, c_dag as CDag, n as N
+import numpy as np
+from pytriqs.operators import c as C, c_dag as CDag, n as N, dagger
 
 from gfoperations import sum
+from transformation import GfStructTransformationIndex
 
 
 class HubbardSite:
@@ -29,3 +31,28 @@ class HubbardPlaquette:
 
     def get_gf_struct(self):
         return [[self.up, range(4)], [self.dn, range(4)]]
+
+
+class HubbardPlaquetteMomentum:
+
+    def __init__(self, u, spins, momenta, transformation):
+        up = spins[0]
+        dn = spins[1]
+        sites = range(4)
+        self.transformation = transformation
+        self.block_labels = [spin+"-"+k for spin in spins for k in momenta]
+        self.gf_struct = [[l, range(1)] for l in self.block_labels]
+        self._to_mom = GfStructTransformationIndex(self.gf_struct, [[up, sites], [dn, sites]])
+        self.h_int = np.sum([u * self._c_dag(up, i) * self._c(up, i) * self._c_dag(dn, i) * self._c(dn, i) for i in sites], axis = 0)
+
+    def _c(self, spin, site):
+        return sum([self.transformation[spin][site, k_index] * C(*self._to_mom(spin, k_index)) for k_index in range(4)])
+
+    def _c_dag(self, spin, site):
+        return dagger(self._c(spin, site))
+
+    def get_h_int(self):
+        return self.h_int
+
+    def get_gf_struct(self):
+        return self.gf_struct

models.py
@@ -1,7 +1,8 @@
 import numpy as np
 from pytriqs.gf.local import BlockGf, GfImFreq, SemiCircular
 
-from hamiltonian import HubbardSite, HubbardPlaquette
+from hamiltonian import HubbardSite, HubbardPlaquette, HubbardPlaquetteMomentum
+from transformation import MatrixTransformation
 
 
 class SingleSiteBethe:
@@ -40,6 +41,43 @@ class PlaquetteBethe:
         self.t_loc = {up: np.array(t_loc), dn: np.array(t_loc)}
         self.mu = {up: mu * np.identity(4), dn: mu * np.identity(4)}
         h = HubbardPlaquette(u, [up, dn])
+        self.h_int = h.get_h_int()
+        self.t = t
+        self.bandwidth = 4 * t
+        self.initial_guess = BlockGf(name_list = [b[0] for b in self.gf_struct],
+                                     block_list = [GfImFreq(n_points = n_iw, beta = beta, indices = b[1]) for b in self.gf_struct])
+        for s, b in self.initial_guess:
+            b << SemiCircular(self.bandwidth * .5)
+
+
+class MomentumPlaquetteBethe:
+
+    def __init__(self, beta, mu, u, tnn_plaquette, tnnn_plaquette, t = 1, n_iw = 1025):
+        self.dim = 2
+        self.beta = beta
+        self.momenta = ["G", "X", "Y", "M"]
+        up = "up"
+        dn = "dn"
+        self.spins = [up, dn]
+        self.sites = range(4)
+        self.block_labels = [spin+"-"+k for spin in self.spins for k in self.momenta]
+        self.gf_struct = [[l, range(1)] for l in self.block_labels]
+        self.gf_struct_site = [[s, self.sites] for s in self.spins]
+        self.u = u
+        transformation_matrix = .5 * np.array([[1,1,1,1],
+                                               [1,-1,1,-1],
+                                               [1,1,-1,-1],
+                                               [1,-1,-1,1]])
+        self.transformation = dict([(s, transformation_matrix) for s in self.spins])
+        mom_transf = MatrixTransformation(self.gf_struct_site, self.transformation, self.gf_struct)
+        a = tnn_plaquette
+        b = tnnn_plaquette
+        t_loc = np.array([[0,a,a,b],[a,0,b,a],[a,b,0,a],[b,a,a,0]])
+        t_loc = {up: np.array(t_loc), dn: np.array(t_loc)}
+        self.t_loc = mom_transf.reblock(mom_transf.transform_matrix(t_loc))
+        mu = {up: mu * np.identity(4), dn: mu * np.identity(4)}
+        self.mu = mom_transf.reblock(mom_transf.transform_matrix(mu))
+        h = HubbardPlaquetteMomentum(u, self.spins, self.momenta, self.transformation)
         self.h_int = h.get_h_int()
         self.t = t
         self.bandwidth = 4 * t