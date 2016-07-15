403ad8db46d36b6398f660d854191ceb05e945f3
Malte Harland <malte@dhcp-172-21-54-44.wlan.uni-hamburg.de.local>
2016-07-15 14:25:28 +0200

    add mixing

--- dmft.py
+++ dmft.py
@@ -16,6 +16,7 @@ class DMFT:
             self.g_loc = self.model.initial_guess.copy()
         else:
             self.g_loc = self.storage.provide_last_g_loc()
+        self._g_loc_old = self.g_loc.copy()
 
     def run_loops(self, n_loops, **parameters_dict):
         self.par.set(parameters_dict)
@@ -23,5 +24,14 @@ class DMFT:
             self.g0.calc_selfconsistency(self.g_loc, self.model.mu)
             self.impurity_solver.run(self.g0.gf, self.model.h_int, **self.par.run_solver())
             self.g_loc = self.impurity_solver.get_g_iw()
+            self.process_impurity_results()
             self.storage.save_loop(self.impurity_solver.get_results(),
                                    {"g_loc_iw": self.g_loc})
+
+    def process_impurity_results(self):
+        self.g_loc = self.mix(self.g_loc, self.par["mix"])
+
+    def mix(self, g, mix):
+        g << mix * self.g_loc + (1 - mix) * self._g_loc_old
+        self._g_loc_old << g
+        return g
