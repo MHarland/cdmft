0754d331e772908d87e69ce1de2277c9ceec49da
Malte Harland <malte@dhcp-172-21-31-106.wlan.uni-hamburg.de.local>
2016-08-22 09:58:42 +0200

    add module evaluation

---
+++ evaluation/generic.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+from bethe.storage import LoopStorage
+
+
+class Evaluation:
+    
+    def __init__(self, archive):
+        self.archive = LoopStorage(archive)
+        self.n_loops = self.archive.disk["dmft_results"]["n_dmft_loops"]
+
+    def get_sign_loop(self):
+        signs = np.empty([self.n_loops]*2)
+        for i in range(self.n_loops):
+            signs[i, 0] = i
+            signs[i, 1] = self.archive.load("average_sign", i)
+        return signs
---
+++ evaluation/nambumomentumplaquettebethe.py
@@ -0,0 +1,26 @@
+import numpy as np, itertools as itt
+
+from bethe.storage import LoopStorage
+from bethe.evaluation.generic import Evaluation as GenericEvaluation
+
+
+class Evaluation(GenericEvaluation):
+    def __init__(self, archive):
+        self.archive = LoopStorage(archive)
+        self.n_loops = self.archive.disk["dmft_results"]["n_dmft_loops"]
+
+    def get_scorderset_loop(self):
+        scoset_loop = np.empty([self.n_loops, 4])
+        for i in range(self.n_loops):
+            g = self.archive.load("g_tau", i)
+            scoset_loop[i, 0] = -g["X"].data[-1,0,1].real
+            scoset_loop[i, 1] = -g["X"].data[-1,1,0].real
+            scoset_loop[i, 2] = -g["Y"].data[-1,0,1].real
+            scoset_loop[i, 3] = -g["Y"].data[-1,1,0].real
+        return scoset_loop
+        
+    def get_scorder_loop(self):
+        scoset_loop = self.get_scorderset_loop()
+        for scoset, j in itt.product(scoset_loop, range(4)):
+            scoset[j] = abs(scoset[j])
+        return np.array([[i, np.mean(scoset_loop[i,:]), np.std(scoset_loop[i,:], ddof=1)] for i in range(self.n_loops)])
