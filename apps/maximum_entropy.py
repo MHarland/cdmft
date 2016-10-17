f0dcd729155d4d0f988dfe038fe241e9c24b2000
Malte Harland <mharland3@gmail.com>
2016-10-17 13:19:37 +0200

    add app maximum entropy

---
+++ apps/maximum_entropy.py
@@ -0,0 +1,35 @@
+import sys
+from pytriqs.archive import HDFArchive
+from pytriqs.gf.local import BlockGf
+from pytriqs.utility import mpi
+from maxent.bryanToTRIQS import MaximumEntropy
+
+from bethe.storage import LoopStorage
+
+
+ntau = 500
+nomega = 1000
+bandwidth = 20
+sigma = 0.003
+par = {"ntau": ntau,
+       "nomega": nomega,
+       "bandwidth": bandwidth,
+       "sigma": sigma}
+for archive_name in sys.argv[1:]:
+    sto = LoopStorage(archive_name)
+    g = sto.load("g_imp_iw")
+    maxent = MaximumEntropy(g, ntau)
+    if sigma:
+        maxent.calculateTotDOS(nomega, bandwidth, sigma)
+    else:
+        maxent.calculateTotDOS(nomega, bandwidth)
+    w = maxent.getOmegaMesh()
+    a = maxent.getTotDOS()
+    if mpi.is_master_node():
+        arch = HDFArchive(archive_name, 'a')
+        if not arch.is_group('maxent_results'):
+            arch.create_group('maxent_results')
+        res = arch['maxent_results']
+        res['mesh'] = w
+        res['a_w'] = a
+        res['parameters'] = par
---
+++ apps/maximum_entropy_plt.py
@@ -0,0 +1,23 @@
+import matplotlib as mpl, sys, numpy as np
+mpl.use("PDF")
+from matplotlib import pyplot as plt
+from pytriqs.archive import HDFArchive
+
+
+fig = plt.figure()
+ax = fig.add_axes([.12,.1,.83,.83])
+n_graphs = len(sys.argv[1:])
+colors = [mpl.cm.jet(float(i)/max(1,n_graphs-1)) for i in range(n_graphs)]
+for archive_name, color in zip(sys.argv[1:], colors):
+    archive = HDFArchive(archive_name, 'r')
+    a_w = archive['maxent_results']['a_w']
+    mesh = archive['maxent_results']['mesh']
+    ax.plot(mesh.real, a_w, label = archive_name[:-3], color = color)
+ax.legend(fontsize = 8)
+ax.set_xlabel("$\\omega$")
+ax.set_ylabel("$A(\\omega)$")
+ax.set_ylim(bottom = 0)
+ax.set_xlim(mesh[0], mesh[-1])
+plt.savefig("maxent.pdf")
+print "maxent.pdf ready"
+plt.close()
