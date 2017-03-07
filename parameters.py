06a496dabe029d638eaf99dc0fb93d4e8e9a0384
MHarland <mharland@li-lin39.PHYSnet.uni-hamburg.de>
2017-03-07 12:01:12 +0100

    add optional parameters for g2

--- parameters.py
+++ parameters.py
@@ -9,7 +9,8 @@ class DMFTParameters:
     def __init__(self, parameter_dict = {}):
         self.solver_run = ["n_cycles", "partition_method", "quantum_numbers", "length_cycle", "n_warmup_cycles", "random_name", "max_time", "verbosity", "move_shift", "move_double", "use_trace_estimator", "measure_g_tau", "measure_g_l", "measure_pert_order", "measure_density_matrix", "use_norm_as_weight", "performance_analysis", "proposal_prob", "imag_threshold", "perform_post_proc", "perform_tail_fit", "fit_min_n", "fit_max_n", "fit_min_w", "fit_max_w", "fit_max_moment", "move_global", "move_global_prob"]
         all_parameternames = ["beta", "n_iw", "n_tau", "n_l", "mix", "make_g0_tau_real", "filling", "block_symmetries", "dmu_max"] + self.solver_run
-        measure_g2_parameters = ["measure_g2_inu", "measure_g2_legendre", "measure_g2_pp", "measure_g2_ph", "measure_g2_block_order", "measure_g2_n_iw", "measure_g2_n_inu", "measure_g2_n_l"]#, "measure_g_pp_tau"]# "measure_g2_blocks"]
+        measure_g2_parameters = ["measure_g2_inu", "measure_g2_legendre", "measure_g2_pp", "measure_g2_ph", "measure_g2_block_order", "measure_g2_n_iw", "measure_g2_n_inu", "measure_g2_n_l", "measure_g_pp_tau", "measure_g2_blocks"]
+        self.optional_parameters = measure_g2_parameters
         self.current = dict([(name, None) for name in all_parameternames])
         self.set(parameter_dict)
 
@@ -57,7 +58,10 @@ class DMFTParameters:
             if par in self.current.keys():
                 self.current[par] = parameter_dict.pop(par)
         if len(parameter_dict) > 0:
-            raise UnkownParameters(parameter_dict)
+            for par in parameter_dict.keys():
+                if not (par in self.optional_parameters):
+                    print par
+                    raise UnkownParameters(parameter_dict)
 
     def __call__(self, parameters = {}):
         self.current.update(parameters)
