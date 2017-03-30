d89b7d87c6b1f1362ed3ec2b47e72c5c8eb57e1e
MHarland <mharland@li-lin39.PHYSnet.uni-hamburg.de>
2017-03-30 16:40:13 +0200

    fix saiao plot to site back-transf

--- apps/saiao_loop_plt.py
+++ apps/saiao_loop_plt.py
@@ -26,7 +26,7 @@ for fname, c in zip(sys.argv[1:], colors):
             n[i, j] = g['spin-site'][i, j].total_density().real
         for s1, s2, i1, i2 in itt.product(range(2), range(2), range(3), range(3)):
             a1, a2 = superindex(s1, i1), superindex(s2, i2)
-            naiao[a1, a2] = np.sum([rots[i1][s1, t1] * rots[i2][t2, s2].conjugate() * sitetransf[i1, k1] * sitetransf[k2, i2].conjugate() * n[superindex(t1, k1), superindex(t2, k2)] for k1, k2, t1, t2 in itt.product(range(3), range(3), range(2), range(2))], axis = 0).real
+            naiao[a1, a2] = np.sum([rots[i1][s1, t1] * rots[i2][t2, s2].conjugate() * sitetransf[k1, i1].conjugate() * sitetransf[i2, k2] * n[superindex(t1, k1), superindex(t2, k2)] for k1, k2, t1, t2 in itt.product(range(3), range(3), range(2), range(2))], axis = 0).real
         saiao = np.sum([naiao[i, i] - naiao[3+i, 3+i] for i in range(3)], axis = 0)
         sz =  np.sum([n[i, i] - n[3+i, 3+i] for i in range(3)], axis = 0)
         y.append(saiao)
