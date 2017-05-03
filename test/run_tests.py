4e0fde6529b3fc3307053811109a56c31a0e0d32
Malte Harland <mharland3@gmail.com>
2017-05-03 19:41:51 +0200

    activate all tests

--- test/run_tests.py
+++ test/run_tests.py
@@ -1,4 +1,5 @@
 import unittest
+from pytriqs.utility import mpi #initializes MPI, required on some clusters
 
 from test_greensfunctions import TestMatsubaraGreensFunction
 from test_parameters import TestDMFTParameters
@@ -51,21 +52,21 @@ suite.addTest(TestSchemesBethe("test_SchemesBethe_calculate"))
 suite.addTest(TestSchemesBethe("test_SchemesBetheAFM_calculate"))
 suite.addTest(TestSchemesBethe("test_SchemesBetheAIAO"))
 suite.addTest(TestSchemesBethe("test_SchemesBethe_find_and_set_mu_single"))
-#suite.addTest(TestSchemesBethe("test_SchemesBethe_find_and_set_mu_double")) # TODO very unstable
+suite.addTest(TestSchemesBethe("test_SchemesBethe_find_and_set_mu_double")) # TODO very unstable
 suite.addTest(TestCycle("test_Cycle_initialization"))
-#suite.addTest(TestCycle("test_Cycle_run"))
+suite.addTest(TestCycle("test_Cycle_run"))
 suite.addTest(TestSetups("test_BetheSetups_init"))
 suite.addTest(TestTightbinding("test_LatticeDispersion_dimer_in_chain"))
 suite.addTest(TestTightbinding("test_LatticeDispersion_dimer_in_chain_transform"))
 suite.addTest(TestTightbinding("test_SquarelatticeDispersion"))
-#suite.addTest(TestSetups("test_SingleBetheSetup_with_cycle_run"))
-#suite.addTest(TestSchemesCDMFT("test_SchemesCDMFT_init"))
-#suite.addTest(TestSchemesCDMFT("test_SchemesCDMFT_dmu"))
-#suite.addTest(TestSchemesCDMFT("test_SchemesCDMFT_calculate_clustersite_basis"))
-#suite.addTest(TestSchemesCDMFT("test_SchemesCDMFT_calculate_clustermomentum_basis"))
-#suite.addTest(TestSchemesCDMFT("test_SchemesCDMFT_Cycle"))
+suite.addTest(TestSetups("test_SingleBetheSetup_with_cycle_run"))
+suite.addTest(TestSchemesCDMFT("test_SchemesCDMFT_init"))
+suite.addTest(TestSchemesCDMFT("test_SchemesCDMFT_dmu"))
+suite.addTest(TestSchemesCDMFT("test_SchemesCDMFT_calculate_clustersite_basis"))
+suite.addTest(TestSchemesCDMFT("test_SchemesCDMFT_calculate_clustermomentum_basis"))
+suite.addTest(TestSchemesCDMFT("test_SchemesCDMFT_Cycle"))
 suite.addTest(TestSetups("test_chain_MomentumDimerCDMFTSetup"))
-#suite.addTest(TestSetups("test_chain_StrelCDMFTSetup"))
-#suite.addTest(TestSetups("test_squarelattice_MomentumPlaquetteCDMFTSetup"))
+suite.addTest(TestSetups("test_chain_StrelCDMFTSetup"))
+suite.addTest(TestSetups("test_squarelattice_MomentumPlaquetteCDMFTSetup"))
 suite.addTest(TestSetups("test_TriangleAIAOBetheSetup"))
 unittest.TextTestRunner(verbosity = 2).run(suite)
